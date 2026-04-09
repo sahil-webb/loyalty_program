18b61e5315f898553d14eb5a0e11aa81bc1770ee
sahil-we <sahilsb7581@gmail.com>
2026-04-09 22:54:50 +0530

    first commit

--- app/routes/api.premium_customer.js
+++ app/routes/api.premium_customer.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { addCustomerPoints } from "pointsLedger";
+import { addCustomerPoints } from "./api.pointsLedger.js";
 
 const prisma = new PrismaClient();
 
