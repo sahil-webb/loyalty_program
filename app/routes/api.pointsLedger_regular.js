61d45209ff58438ebf3bab65e0998c52738c95ff
sahil-we <sahilsb7581@gmail.com>
2026-04-11 01:05:55 +0530

    first commit

--- app/routes/api.pointsLedger_regular.js
+++ app/routes/api.pointsLedger_regular.js
@@ -1,4 +1,6 @@
-import prisma from "../db.server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 /*
 ========================================================
